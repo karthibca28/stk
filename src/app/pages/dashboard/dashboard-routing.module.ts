d18ffc80064d2eb8678885d179e067bf93d46e58
KarthiKumar <karthibca28@gmail.com>
2023-10-17 15:19:23 +0530

    location and expoloereneddremove and fun added

--- src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddNewLocationComponent } from './add-new-location/add-new-location.component';
-import { AddPersonBlacklistComponent } from './add-person-blacklist/add-person-blacklist.component';
-import { AddVehicleBlacklistComponent } from './add-vehicle-blacklist/add-vehicle-blacklist.component';
 import { ApproveLocationComponent } from './approve-location/approve-location.component';
 import { BroadcastMessageViewComponent } from './broadcast-message-view/broadcast-message-view.component';
 import { BroadcastMessageComponent } from './broadcast-message/broadcast-message.component';
-import { ExploreAreaComponent } from './explore-area/explore-area.component';
 import { HomeComponent } from './home/home.component';
 import { ImeiSearchComponent } from './imei-search/imei-search.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -30,10 +27,6 @@ const routes: Routes = [
     component:ApproveLocationComponent
   },
   {
-    path:'explore-area',
-    component:ExploreAreaComponent
-  },
-  {
     path:'broadcast-message',
     component:BroadcastMessageComponent
   },
@@ -42,24 +35,8 @@ const routes: Routes = [
     component:BroadcastMessageViewComponent
   },
   {
-    path:'add-person-blacklist',
-    component:AddPersonBlacklistComponent
-  },
-  {
-    path:'add-person-blacklist/:personBId',
-    component:AddPersonBlacklistComponent
-  },
-  {
     path:'vehicle-blacklist',
     component:VehicleBlacklistComponent
-  },
-  {
-    path:'add-vehicle-blacklist',
-    component:AddVehicleBlacklistComponent
-  },
-  {
-    path:'add-vehicle-blacklist/:vehicleBId',
-    component:AddVehicleBlacklistComponent
   },
   {
     path:'profile',
